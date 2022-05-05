a4b0d9d7cdf45203bf6f743aa4842a200ca72844
Ariel Campos <ariel.campos94@yahoo.com>
2022-05-05 18:34:22 -0300

    decodificador finalizado

--- app.js
+++ app.js
@@ -5,15 +5,14 @@ const txtOutput           = document.querySelector(".mensagem");
 var   textArea            = document.getElementById("textOutput");
 
 function btnEncriptar() {
-    const textoRecebido = txtInput.value;;
+    const textoRecebido = txtInput.value;
     var textoEncriptado = encriptar(textoRecebido);
-    escondeImg();
+    exibicaoImg();
     txtOutput.value = textoEncriptado;
-
 }
 
 function encriptar(texto) {
-    let matrizCodigo = [["e", "enter"], ["o", "ober"], ["u", "ufat"], ["i", "imes"], ["a", "ai"]];
+    let matrizCodigo = [["e", "enter"], ["o", "ober"], ["i", "imes"], ["a", "ai"], ["u", "ufat"]];
     texto = texto.toLowerCase(); // Transforma texto em minusculo
 
     for (let i = 0; i < matrizCodigo.length; i++) {
@@ -25,11 +24,32 @@ function encriptar(texto) {
     return texto;
 }
 
-function escondeImg () {
+function btnDesencriptar() {
+    const textoRecebido = txtInput.value;
+    var textoDesencriptado = desencriptar(textoRecebido);
+    exibicaoImg();
+    txtOutput.value = textoDesencriptado;
+}
+
+function desencriptar(texto) {
+    let matrizCodigo = [["enter", "e"], ["ober", "o"], ["imes", "i"], ["ai", "a"], ["ufat", "u"]];
+    texto = texto.toLowerCase(); // Transforma texto em minusculo
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (texto.includes(matrizCodigo[i][0])) {
+            texto = texto.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+
+    return texto;
+}
+
+function exibicaoImg(texto) {
     textArea.classList.remove("bg-img");
 }
 
 btn_criptografar.addEventListener('click', btnEncriptar);
+btn_descriptografar.addEventListener('click', btnDesencriptar);
 
 
 
