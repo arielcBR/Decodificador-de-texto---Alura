32ff70b7ff9ef84e887205d1691196f05ab53876
Ariel Campos <ariel.campos94@yahoo.com>
2022-05-06 15:58:57 -0300

    Botoes de copiar texto finalizados

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const btn_criptografar    = document.getElementById("botao_criptografar");
+const btn_criptografar = document.getElementById("botao_criptografar");
 const btn_descriptografar = document.getElementById("botao_descriptografar");
-const btn_copy_input      = document.getElementById("btn-copy_input");
-const btn_copy_output     = document.getElementById("btn-copy_output");
-const txtInput            = document.querySelector("#textInput");
-const txtOutput           = document.querySelector("#textOutput");
+const btn_copy_input = document.getElementById("btn-copy_input");
+const btn_copy_output = document.getElementById("btn-copy_output");
+const txtInput = document.querySelector("#textInput");
+const txtOutput = document.querySelector("#msgTraduzida");
 
 function btnEncriptar() {
     const textoRecebido = txtInput.value;
     var textoEncriptado = encriptar(textoRecebido);
-    exibicaoImg();
-    txtOutput.value = textoEncriptado;
+    escondeInfo();
+    mostraBotaoCopy();
+    txtOutput.textContent = textoEncriptado;
 }
 
 function encriptar(texto) {
     let matrizCodigo = [["e", "enter"], ["o", "ober"], ["i", "imes"], ["a", "ai"], ["u", "ufat"]];
-    texto = texto.toLowerCase(); 
+    texto = texto.toLowerCase();
 
     for (let i = 0; i < matrizCodigo.length; i++) {
         if (texto.includes(matrizCodigo[i][0])) {
@@ -28,8 +29,9 @@ function encriptar(texto) {
 function btnDesencriptar() {
     const textoRecebido = txtInput.value;
     var textoDesencriptado = desencriptar(textoRecebido);
-    exibicaoImg();
-    txtOutput.value = textoDesencriptado;
+    escondeInfo();
+    mostraBotaoCopy();
+    txtOutput.textContent = textoDesencriptado;
 }
 
 function desencriptar(texto) {
@@ -45,9 +47,14 @@ function desencriptar(texto) {
     return texto;
 }
 
-function exibicaoImg() {
-    let textArea = document.getElementById("textOutput");
-    textArea.classList.remove("bg-img");
+function escondeInfo() {
+    let containerInfo = document.getElementById("info");
+    containerInfo.classList.add("esconde");
+}
+
+function mostraBotaoCopy() {
+    let botaoCopy = document.getElementById("btn-copy_output");
+    botaoCopy.classList.add("exibe");
 }
 
 btn_criptografar.addEventListener('click', btnEncriptar);
@@ -55,13 +62,11 @@ btn_criptografar.addEventListener('click', btnEncriptar);
 btn_descriptografar.addEventListener('click', btnDesencriptar);
 
 btn_copy_input.addEventListener('click', () => {
-    txtInput.select();
-    document.execCommand("Copy");
+    let copiaTexto = txtInput.value;
+    navigator.clipboard.writeText(copiaTexto);
 })
 
 btn_copy_output.addEventListener('click', () => {
-    txtOutput.select();
-    document.execCommand("Copy");
+    let copiaTexto = txtOutput.textContent;
+    navigator.clipboard.writeText(copiaTexto);
 })
-
-
